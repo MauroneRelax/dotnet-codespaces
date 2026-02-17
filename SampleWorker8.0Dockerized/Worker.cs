cec16abe932d685d8d47b473e2b3cdaebd82ec6b
MauroneRelax <m.poletto.mobile@live.it>
2026-02-17 00:58:22 +0000

    worker

---
+++ SampleWorker8.0Dockerized/Worker.cs
@@ -0,0 +1,16 @@
+namespace SampleWorker8._0Dockerized;
+
+public class Worker(ILogger<Worker> logger) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (logger.IsEnabled(LogLevel.Information))
+            {
+                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            }
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
